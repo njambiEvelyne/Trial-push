e11b067b516fb590b984ba4c367af21ac4fc87cd
njambiEvelyne <evelynenganga76@gmail.com>
2024-12-17 21:35:14 +0300

    Added for

--- Py.py
+++ Py.py
@@ -1,2 +1,7 @@
 print("This is a new file\n")
 print('I am so excited as the configuration has effectively worked!')
+
+fruits = ["banana", "Oranges", "Apples"]
+for fruit in fruits:
+  print(fruit)
+  print(len(fruit))
