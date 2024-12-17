68de4844a3aa55549a2315d3a53c277848e76a68
njambiEvelyne <evelynenganga76@gmail.com>
2024-12-17 22:55:00 +0300

    Added a file

--- sum.py
+++ sum.py
@@ -10,3 +10,19 @@ for number in numbers:
 
 # After the loop, print the final total value
 print("The sum of all numbers in the list is:", total)
+
+languages = ['Swift', 'Python', 'Go']
+
+# Start of loop
+for lang in languages:
+    print(lang)
+    print('-----')
+# End of for loop
+
+print('Last statement')
+
+language = 'Python'
+
+# iterate over each character in language
+for x in language:
+    print(x)
