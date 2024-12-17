893879a16659f94c8bef85de3ef9e2f76c65aad2
njambiEvelyne <evelynenganga76@gmail.com>
2024-12-17 22:32:34 +0300

    Added a file as well as txt in it!

---
+++ list.py
@@ -0,0 +1,20 @@
+# number = input("Enetr a list of numbers: ")
+# count = 0
+# for number in range :
+#   if number % 2 == 0:
+#     count += 1
+#     print(f"The evn numbers are{number}")
+# Given list
+input_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# # Using list comprehension to get even numbers
+# output_list = [number for number in input_list if number % 2 == 0]
+
+# # Printing the result
+# print(output_list)
+input_list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+output_list = [number for number in input_list if number %2 == 0]
+
+print(output_list)
+
