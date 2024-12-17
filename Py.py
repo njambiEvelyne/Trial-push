9f63f6f05e37963d10e528dd561e56299fe03446
njambiEvelyne <evelynenganga76@gmail.com>
2024-12-17 21:39:45 +0300

    Added content

--- Py.py
+++ Py.py
@@ -1,7 +1,15 @@
 print("This is a new file\n")
 print('I am so excited as the configuration has effectively worked!')
 
+# Iterating over lists
 fruits = ["banana", "Oranges", "Apples"]
 for fruit in fruits:
   print(fruit)
   print(len(fruit))
+
+# Working over the Tuples
+colors = ("red", "green", "blue")
+
+for color in colors:
+  print(color)
+  
