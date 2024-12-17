727b724c7b2d00b89a0e9451f7b2cb8b9bd94acd
njambiEvelyne <evelynenganga76@gmail.com>
2024-12-17 21:55:14 +0300

    Added a file

--- Py.py
+++ Py.py
@@ -9,7 +9,14 @@ for fruit in fruits:
 
 # Working over the Tuples
 colors = ("red", "green", "blue")
-
 for color in colors:
   print(color)
-  
+
+# Iterating through the characters in a string
+message = "Hello world!"  
+for character in message:
+  print(f"The character is :{character}")
+
+# Iterating over range
+for number in range(1,6)  :
+  print(f"The numbers are: {number}")
---
+++ messae.py
@@ -0,0 +1,12 @@
+successful = False
+
+for number in range(3):
+  print("Attempt", number )
+  if successful:
+    print("Successful")
+    break
+else:
+  print("Attempted three times and failed")
+
+
+  
